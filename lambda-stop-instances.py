9721cc403e7e788b015cb895a459a50af01519b2
Armando <aalorro@gmail.com>
2020-05-23 16:03:25 -0400

    more changes

--- lambda-stop-instances.py
+++ lambda-stop-instances.py
@@ -1,7 +1,7 @@
 import boto3
 
 region = 'us-east-2'
-instances = ['i-0b8e04c02e8856c5a'] # you may input as many instances in the list
+instances = ['i-0b8e04c02e8856c5a'] # you may input as many instances in the list   
 
 ec1 = boto3.client('ec2', region_name=region)
 
